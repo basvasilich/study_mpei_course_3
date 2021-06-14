b48768d902431ac7d582ea1c55298879956c9055
Vadim Patsev <basvasilich@yandex.ru>
2021-06-14 19:04:02 +0300

    initial

--- study_mpei_course_3/settings.py
+++ study_mpei_course_3/settings.py
@@ -115,7 +115,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 )
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
