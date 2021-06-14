46f92b408baca7256b9c897690c30ca6590f7bea
Vadim Patsev <basvasilich@yandex.ru>
2021-06-14 18:42:41 +0300

    initial

--- study_mpei_course_3/settings.py
+++ study_mpei_course_3/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@mega3$cie*#%y5@08-2+f+5v&s!$i+t15fr346xrm5zbfq*&g'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,17 +75,7 @@ WSGI_APPLICATION = 'study_mpei_course_3.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dbn8mj6ko0sr1u',
-        'USER': 'ijxawwdzxyfllp',
-        'PASSWORD': '6096c7ae22456b166123a892f4754c7fb40afc0a22e17a2fef0467a638a774f3',
-        'HOST': 'ec2-34-242-89-204.eu-west-1.compute.amazonaws.com',
-        'PORT': '5432',
-    }
-}
-
+DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600)}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
